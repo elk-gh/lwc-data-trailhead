d9aa3e49440b67419e11a89b7d5b13ba31ef9240
elk-gh <elkin.cordobab@gmail.com>
2020-06-28 02:00:57 -0500

    added ldsUtils

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRST_NAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Last Name', fieldName: LAST_NAME_FIELD.fieldApiName, type: 'text' },
