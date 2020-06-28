06ce7f40e1f74a3c34ef9a4a2e3ae20b58548fac
elk-gh <elkin.cordobab@gmail.com>
2020-06-28 01:05:47 -0500

    corrected fileds used in record form The mismatch caused and error

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -1,9 +1,9 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
-import FIRST_NAME_FIELD from '@salesforce/schema/Account.Name';
-import LAST_NAME_FIELD from '@salesforce/schema/Account.AnnualRevenue';
-import EMAIL_FIELD from '@salesforce/schema/Account.Industry';
+import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 
 export default class ContactCreator extends LightningElement {
     objectApiName = CONTACT_OBJECT;
